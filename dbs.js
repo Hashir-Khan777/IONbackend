36599280c5acc20a3bb3c122f196b2edf9d547bc
Hashir Khan <khnhashir555@gmail.com>
2024-07-01 19:24:44 +0500

    ion backend

--- dbs.js
+++ dbs.js
@@ -1,21 +1,18 @@
 module.exports = {
   DEVELOPMENT: {
     username: "avnadmin",
-    password: "AVNS_B8fk_9sK0YA7oE2r1U2",
     database: "inventonbtc",
     host: "pg-f0e7207-hashirk966-d8b9.f.aivencloud.com",
     port: 12822,
   },
   TEST: {
     username: "avnadmin",
-    password: "AVNS_B8fk_9sK0YA7oE2r1U2",
     database: "inventonbtc",
     host: "pg-f0e7207-hashirk966-d8b9.f.aivencloud.com",
     port: 12822,
   },
   PRODUCTION: {
     username: "avnadmin",
-    password: "AVNS_B8fk_9sK0YA7oE2r1U2",
     database: "inventonbtc",
     host: "pg-f0e7207-hashirk966-d8b9.f.aivencloud.com",
     port: 12822,
